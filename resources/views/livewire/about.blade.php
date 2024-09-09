7835424a4155eb5b95eeebbe9ad3118ba3e641e5
Neardey Loem <lneardey@prosethsolutions.com>
2024-09-09 17:10:58 +0700

    design interface about page

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -1,8 +1,283 @@
 <x-guest-layout>
-    <div class="h-56 w-screen bg-red-600 mt-10 py-11">
-        <h1>Hello from about page</h1>
-    </div>
-    <div class="h-10 w-screen bg-green-900">
-        <h1>Hello world</h1>
-    </div>
+    <section class="py-16 bg-gray-50">
+        <div class="about-card my-20 mx-20 flex gap-6 p-5">
+            <div class="card-item w-full md:w-1/3">
+                <img src="https://memberclicks.com/wp-content/uploads/2021/12/membership-certificate-1-scaled.jpg"
+                    alt="Membership certificate" class="w-full h-full rounded-sm">
+            </div>
+            <div class="card-item w-full md:w-1/3">
+                <img src="https://elearningindustry.com/wp-content/uploads/2015/03/shutterstock_182148977.jpg"
+                    alt="E-learning industry" class="w-full h-full rounded-sm">
+            </div>
+            <div class="card-item w-full md:w-1/3">
+                <img src="https://media.licdn.com/dms/image/D4D12AQFu2pW5YLYVlw/article-cover_image-shrink_720_1280/0/1667981095211?e=2147483647&v=beta&t=4d7FaCMYPh7SjX0nYBMqqD5pZeVgguBPfgXoXeMNrpc"
+                    alt="LinkedIn article cover" class="w-full h-full rounded-sm">
+            </div>
+            <div class="card-item w-full md:w-1/3">
+                <img src="https://www.vodacom.co.za/sites/vodacomcoza/files/2024-03/e-learning-123-stepper.png"
+                    alt="Base64 image example" class="w-full h-full rounded-sm">
+            </div>
+        </div>
+
+        <div class="text-center mt-5 mb-12">
+            <h2 class="text-4xl font-bold text-gray-800">The more you learn, The more you earn</h2>
+            <p class="mt-4 text-gray-600">By expanding our knowledge and skills, we not only increase our earning
+                potential but also enhance our overall quality of life.</p>
+        </div>
+
+        <div class="my-10 h-full mx-20 flex justify-evenly gap-8">
+            <div class="card ">
+                <div class="bg ">
+                    <div class="card-icon mt-5 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 16 16">
+                            <path fill="#d28c14"
+                                d="m13.637 2.363l1.676.335c.09.018.164.084.19.173a.25.25 0 0 1-.062.249l-1.373 1.374a.88.88 0 0 1-.619.256H12.31L9.45 7.611A1.5 1.5 0 1 1 6.5 8a1.5 1.5 0 0 1 1.889-1.449l2.861-2.862V2.552c0-.232.092-.455.256-.619L12.88.559a.25.25 0 0 1 .249-.062c.089.026.155.1.173.19Z" />
+                            <path fill="#d28c14"
+                                d="M2 8a6 6 0 1 0 11.769-1.656a.751.751 0 1 1 1.442-.413a7.502 7.502 0 0 1-12.513 7.371A7.501 7.501 0 0 1 10.069.789a.75.75 0 0 1-.413 1.442A6 6 0 0 0 2 8" />
+                            <path fill="#d28c14"
+                                d="M5 8a3.002 3.002 0 0 0 4.699 2.476a3 3 0 0 0 1.28-2.827a.748.748 0 0 1 1.045-.782a.75.75 0 0 1 .445.61A4.5 4.5 0 1 1 8.516 3.53a.75.75 0 1 1-.17 1.49A3 3 0 0 0 5 8" />
+                        </svg>
+                    </div>
+                    <div class="card-title text-center mt-4">
+                        <h1 class="fw-bold">Vission</h1>
+                    </div>
+                    <div class="card-body mt-2 mx-5">
+                        <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sit?</p>
+                    </div>
+                </div>
+                <div class="blob"></div>
+            </div>
+            <div class="card">
+                <div class="bg ">
+                    <div class="card-icon mt-5 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
+                            <path fill="none" stroke="#d25614" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="1.5"
+                                d="M12.107 18.251a2.063 2.063 0 1 0 0-4.126a2.063 2.063 0 0 0 0 4.126m3.656 4.874a3.656 3.656 0 0 0-7.312 0m10.902-7.481a2.017 2.017 0 1 0 0-4.034a2.017 2.017 0 0 0 0 4.034m3.576 4.767A3.575 3.575 0 0 0 17 17.719M4.647 15.644a2.017 2.017 0 1 0 0-4.034a2.017 2.017 0 0 0 0 4.034m-3.576 4.767A3.575 3.575 0 0 1 7 17.719m5-8.594a3 3 0 1 0 0-6a3 3 0 0 0 0 6m-.5-8.25h1m-.5 0v2.25m3.359-1.066l.707.707m-.354-.353l-1.591 1.591m3.129 1.621v1m0-.5H15m1.066 3.359l-.707.707m.353-.354l-1.591-1.591M12.5 11.375h-1m.5 0v-2.25m-3.359 1.066l-.707-.707m.354.353l1.591-1.591M6.75 6.625v-1m0 .5H9M7.934 2.766l.707-.707m-.353.354l1.591 1.591" />
+                        </svg>
+                    </div>
+                    <div class="card-title text-center mt-4">
+                        <h1 class="fw-bold">Mission</h1>
+                    </div>
+                    <div class="card-body mt-2 mx-5">
+                        <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sit?</p>
+                    </div>
+                </div>
+                <div class="blob"></div>
+            </div>
+            <div class="card">
+                <div class="bg ">
+                    <div class="card-icon mt-5 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 32 32">
+                            <path fill="#0a72b2"
+                                d="M26 30h-2v-3a5.006 5.006 0 0 0-5-5h-6a5.006 5.006 0 0 0-5 5v3H6v-3a7.01 7.01 0 0 1 7-7h6a7.01 7.01 0 0 1 7 7zM5 6a1 1 0 0 0-1 1v9h2V7a1 1 0 0 0-1-1" />
+                            <path fill="#0a72b2"
+                                d="M4 2v2h5v7a7 7 0 0 0 14 0V4h5V2Zm7 2h10v3H11Zm5 12a5 5 0 0 1-5-5V9h10v2a5 5 0 0 1-5 5" />
+                        </svg>
+                    </div>
+                    <div class="card-title text-center mt-4">
+                        <h1 class="fw-bold">Core Value</h1>
+                    </div>
+                    <div class="card-body mt-2 mx-5">
+                        <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sit?</p>
+                    </div>
+                </div>
+                <div class="blob"></div>
+            </div>
+        </div>
+
+        <div class="text-center mb-12">
+            <h2 class="text-4xl font-bold text-gray-800">Why Choose E-learning</h2>
+            <p class="mt-4 text-gray-600">Look into yourself, know you're ambitious and keep moving forward until you
+                get something in return as your achievement.</p>
+        </div>
+
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
+
+            <!-- Card 1 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-gray-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <!-- Replace with actual icon -->
+                    {{-- <img src="/path-to-icon1.svg" alt="Icon" class="w-8 h-8"> --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 28 28">
+                        <path fill="#1192ac"
+                            d="M12 5.5a2 2 0 0 0 1.491 1.935c.337.053.68.053 1.018 0A2 2 0 1 0 12 5.5m-1.337 1.058a3.5 3.5 0 1 1 6.675 0l4.419-1.436a2.477 2.477 0 1 1 1.53 4.712L18 11.552v1.598a7.4 7.4 0 0 0-1.5.474V11.37c0-.541.349-1.021.864-1.189l5.46-1.774a.977.977 0 0 0-.604-1.859l-6.752 2.194q-.32.104-.649.162a3.5 3.5 0 0 1-1.639 0a5 5 0 0 1-.648-.162L5.78 6.548a.977.977 0 0 0-.604 1.859l5.46 1.774c.515.168.864.648.864 1.189v3.957c0 .35-.067.698-.198 1.024l-2.734 6.811a.977.977 0 0 0 .538 1.267a.96.96 0 0 0 1.252-.531l1.684-4.196a7.6 7.6 0 0 0 .342 3.174l-.634 1.58a2.46 2.46 0 0 1-3.21 1.363a2.477 2.477 0 0 1-1.364-3.215l2.734-6.812q.09-.224.09-.466v-3.774L4.712 9.834a2.477 2.477 0 0 1 1.531-4.712zM26 20.5a6.5 6.5 0 1 1-13 0a6.5 6.5 0 0 1 13 0m-2.646-2.854a.5.5 0 0 0-.708 0L18 22.293l-1.646-1.647a.5.5 0 0 0-.708.708l2 2a.5.5 0 0 0 .708 0l5-5a.5.5 0 0 0 0-.708" />
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Course Accessibility</h3>
+                <p class="text-gray-600">Select a suitable course from the vast area of other courses and access it
+                    anytime and from anywhere.</p>
+            </div>
+
+            <!-- Card 2 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-blue-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 1024 1024">
+                        <path fill="#1192ac"
+                            d="M384 960v-64h192.064v64zm448-544a350.66 350.66 0 0 1-128.32 271.424C665.344 719.04 640 763.776 640 813.504V832H320v-14.336c0-48-19.392-95.36-57.216-124.992a351.55 351.55 0 0 1-128.448-344.256c25.344-136.448 133.888-248.128 269.76-276.48A352.384 352.384 0 0 1 832 416m-544 32c0-132.288 75.904-224 192-224v-64c-154.432 0-256 122.752-256 288z" />
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Scholarship</h3>
+                <p class="text-gray-600">To encourage talent, we give up to 100% aid to those young learners who have
+                    the ability to do something.</p>
+            </div>
+
+            <!-- Card 3 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-orange-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
+                        <g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                            color="#1192ac">
+                            <path stroke="#1192ac"
+                                d="M12 15.5v5M5.98 3.285c-1.692-.322-2.538-.483-3.26.135C2 4.037 2 5.04 2 7.046v7.209c0 1.834 0 2.751.463 3.324c.462.572 1.48.766 3.518 1.154c1.815.346 3.232.896 4.258 1.45c1.01.545 1.514.817 1.761.817s.752-.272 1.76-.817c1.027-.553 2.444-1.104 4.26-1.45c2.036-.388 3.055-.582 3.517-1.154c.463-.573.463-1.49.463-3.324V7.046c0-2.006 0-3.009-.72-3.626c-.722-.618-1.568-.457-3.26-.135" />
+                            <path fill="#1192ac"
+                                d="m9.293 10.707l.53-.53zm5.414 0l-.53-.53zm0-5.414l-.53.53zM11.25 11a.75.75 0 0 0-1.5 0zm-1.5 2a.75.75 0 0 0 1.5 0zm4.5-2a.75.75 0 0 0-1.5 0zm-1.5 2a.75.75 0 0 0 1.5 0zm-1.5-10a.75.75 0 0 0-1.5 0zm-1.5 2a.75.75 0 0 0 1.5 0zm4.5-2a.75.75 0 0 0-1.5 0zm-1.5 2a.75.75 0 0 0 1.5 0zM9 7.25a.75.75 0 0 0 0-1.5zm-2-1.5a.75.75 0 0 0 0 1.5zm2 4.5a.75.75 0 0 0 0-1.5zm-2-1.5a.75.75 0 0 0 0 1.5zm10-1.5a.75.75 0 0 0 0-1.5zm-2-1.5a.75.75 0 0 0 0 1.5zm2 4.5a.75.75 0 0 0 0-1.5zm-2-1.5a.75.75 0 0 0 0 1.5zm-4-3h2v-1.5h-2zM14.25 7v2h1.5V7zM13 10.25h-2v1.5h2zM9.75 9V7h-1.5v2zM11 10.25c-.493 0-.787-.002-.997-.03a.7.7 0 0 1-.18-.043l-1.06 1.06c.309.31.684.422 1.04.47c.334.045.747.043 1.197.043zM8.25 9c0 .45-.002.863.043 1.197c.048.356.16.731.47 1.04l1.06-1.06l-.001-.003l-.005-.01a.7.7 0 0 1-.037-.167c-.028-.21-.03-.504-.03-.997zm6 0c0 .493-.002.787-.03.997a.7.7 0 0 1-.043.18l1.06 1.06c.31-.309.422-.684.47-1.04c.045-.334.043-.747.043-1.197zM13 11.75c.45 0 .863.002 1.197-.043c.356-.048.731-.16 1.04-.47l-1.06-1.06l-.003.001l-.01.005a.7.7 0 0 1-.167.037c-.21.028-.504.03-.997.03zm0-6c.493 0 .787.002.997.03a.7.7 0 0 1 .177.042l.003.001l1.06-1.06c-.309-.31-.684-.422-1.04-.47c-.334-.045-.747-.043-1.197-.043zM15.75 7c0-.45.002-.863-.043-1.197c-.048-.356-.16-.731-.47-1.04l-1.06 1.06l.001.003l.005.01c.009.022.024.07.037.167c.028.21.03.504.03.997zM11 4.25c-.45 0-.863-.002-1.197.043c-.356.048-.731.16-1.04.47l1.06 1.06l.003-.001l.01-.005a.7.7 0 0 1 .167-.037c.21-.028.504-.03.997-.03zM9.75 7c0-.493.002-.787.03-.997a.7.7 0 0 1 .043-.18l-1.06-1.06c-.31.309-.422.684-.47 1.04c-.045.334-.043.747-.043 1.197zm0 4v2h1.5v-2zm3 0v2h1.5v-2zm-3-8v2h1.5V3zm3 0v2h1.5V3zM9 5.75H7v1.5h2zm0 3H7v1.5h2zm8-3h-2v1.5h2zm0 3h-2v1.5h2z" />
+                        </g>
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Practical Learning</h3>
+                <p class="text-gray-600">Interact yourself with the real-world while doing the real-world project and
+                    other things to master your skills.</p>
+            </div>
+
+            <!-- Card 4 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-green-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 32 32">
+                        <path fill="#1192ac"
+                            d="M23 17v-2h-2.1c-.1-.6-.4-1.2-.7-1.8l1.5-1.5l-1.4-1.4l-1.5 1.5c-.5-.3-1.1-.6-1.8-.7V9h-2v2.1c-.6.1-1.2.4-1.8.7l-1.5-1.5l-1.4 1.4l1.5 1.5c-.3.5-.6 1.1-.7 1.8H9v2h2.1c.1.6.4 1.2.7 1.8l-1.5 1.5l1.4 1.4l1.5-1.5c.5.3 1.1.6 1.8.7V23h2v-2.1c.6-.1 1.2-.4 1.8-.7l1.5 1.5l1.4-1.4l-1.5-1.5c.3-.5.6-1.1.7-1.8zm-7 2c-1.7 0-3-1.3-3-3s1.3-3 3-3s3 1.3 3 3s-1.3 3-3 3" />
+                        <path fill="#1192ac"
+                            d="M16 2v2c6.6 0 12 5.4 12 12s-5.4 12-12 12v2c7.7 0 14-6.3 14-14S23.7 2 16 2M8.2 25.1L7 26.7c1.2 1 2.6 1.9 4.2 2.4l.7-1.9c-1.4-.5-2.6-1.2-3.7-2.1m-4-7.1l-2 .4c.3 1.6.9 3.2 1.7 4.6l1.7-1c-.7-1.2-1.2-2.6-1.4-4m1.4-8L3.9 9c-.8 1.4-1.4 3-1.6 4.6l2 .3c.1-1.4.6-2.7 1.3-3.9m6.2-5.2l-.7-1.9C9.6 3.5 8.2 4.3 7 5.3l1.3 1.5c1-.8 2.2-1.5 3.5-2" />
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Expert Instructions</h3>
+                <p class="text-gray-600">Hold the opportunity to learn from the industry's expert and learn how to
+                    execute things like them.</p>
+            </div>
+
+            <!-- Card 5 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-purple-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
+                        <path fill="none" stroke="#1192ac" stroke-width="2"
+                            d="M17 7h6v16H7v-4m16-8h-6M13 0v3M1 7h16M1 3h16v16H1zm4-3v3m-1 8h2m2 0h6M4 15h2m2 0h6" />
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Schedule Learning</h3>
+                <p class="text-gray-600">Learn at whatever and whenever at your suitable time and place. Get a
+                    part-time
+                    study degree.</p>
+            </div>
+
+            <!-- Card 6 -->
+            <div class="bg-white rounded-lg shadow-lg p-6 text-center">
+                <div class="bg-pink-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 48 48">
+                        <g fill="none" stroke="#1192ac" stroke-linecap="round" stroke-linejoin="round">
+                            <path
+                                d="M38.5 5.5h-29c-2.2 0-4 1.8-4 4v29c0 2.2 1.8 4 4 4h29c2.2 0 4-1.8 4-4v-29c0-2.2-1.8-4-4-4m.5 10.3v-3c0-2.2-1.8-4-4-4h-5" />
+                            <path d="m32.5 24l-15 8.6V15.4zM8.7 36.5v2.1c0 .5.4.8.8.8h2.9" />
+                        </g>
+                    </svg>
+                </div>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Recorded Sessions</h3>
+                <p class="text-gray-600">Missed the live class? Don’t worry about it, access to every session on the
+                    chosen course.</p>
+            </div>
+
+        </div>
+
+    </section>
+
 </x-guest-layout>
+
+
+
+<style>
+    /* card-image */
+    .about-card {
+        overflow: auto;
+        display: flex;
+        scroll-snap-type: x mandatory;
+        width: 90%;
+        margin: 0 auto;
+        /* padding: 0 15px; */
+    }
+
+    .card-item {
+        background: rgba(255, 255, 255, 0.25);
+        box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+        backdrop-filter: blur(7px);
+        -webkit-backdrop-filter: blur(7px);
+    }
+    /* ================= */
+
+    .card {
+        position: relative;
+        width: 310px;
+        height: 250px;
+        border-radius: 14px;
+        z-index: 1;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
+
+        ;
+    }
+
+    .bg {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        width: 300px;
+        height: 240px;
+        z-index: 2;
+        background: rgba(255, 255, 255, .95);
+        backdrop-filter: blur(24px);
+        border-radius: 10px;
+        overflow: hidden;
+        outline: 2px solid white;
+    }
+
+    .blob {
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        left: 50%;
+        width: 190px;
+        height: 150px;
+        border-radius: 50%;
+        background-color: blue;
+        opacity: 1;
+        filter: blur(12px);
+        animation: blob-bounce 5s infinite ease;
+    }
+
+    @keyframes blob-bounce {
+        0% {
+            transform: translate(-100%, -100%) translate3d(0, 0, 0);
+        }
+
+        25% {
+            transform: translate(-100%, -100%) translate3d(100%, 0, 0);
+        }
+
+        50% {
+            transform: translate(-100%, -100%) translate3d(100%, 100%, 0);
+        }
+
+        75% {
+            transform: translate(-100%, -100%) translate3d(0, 100%, 0);
+        }
+
+        100% {
+            transform: translate(-100%, -100%) translate3d(0, 0, 0);
+        }
+    }
+
+
+
+</style>
